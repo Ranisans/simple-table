66733c5b276fdde8fca1e3793854898da4b69880
Ranisans <ranisans@yahoo.com>
2020-07-23 16:47:14 +0300

    feat: add spinner

---
+++ src/components/Spinner/spinner.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Spinner from './index';
+
+export default {
+  title: 'Spinner',
+  component: Spinner,
+};
+
+export const Base = () => <div style={{ height: '100vh', width: '100vw' }}><Spinner/></div>;
