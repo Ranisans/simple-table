14cc59d43b133fb4ab283d17b1ff7ba526cbef63
Ranisans <ranisans@yahoo.com>
2020-07-24 12:30:52 +0300

    feat: add HeaderCell add FilterContext

---
+++ src/components/TableHeader/HeaderCell.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import FilterContext from '../filterContext';
+
+import './HeaderCell.scss';
+
+const HeaderCell = ({ cellClass, label }) => {
+  const { activeFilter, toggleActiveFilter } = React.useContext(FilterContext);
+  const [isFilterIncrease, setIsFilterIncrease] = React.useState(null);
+
+  const clickHandler = () => {
+    if (isFilterIncrease === null) {
+      setIsFilterIncrease(true);
+      toggleActiveFilter({
+        activeClassCell: cellClass,
+        increase: true,
+      });
+    } else {
+      setIsFilterIncrease(!isFilterIncrease);
+      toggleActiveFilter({
+        activeClassCell: cellClass,
+        increase: !isFilterIncrease,
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    if (activeFilter.activeClassCell !== cellClass && isFilterIncrease !== null) {
+      setIsFilterIncrease(null);
+    }
+  }, [activeFilter]);
+
+  return (
+    <FilterContext.Consumer>
+    {() => (
+      <div className={`header_cell ${cellClass}`} onClick={clickHandler}>
+        <span>{label}</span>
+        <span className='arrowBlock'>
+          <span className={isFilterIncrease === true ? 'active' : 'disabled'}>
+            <svg viewBox="0 0 1024 1024" focusable="false" width="1em" height="1em">
+              <path d="M858.9 689L530.5 308.2c-9.4-10.9-27.5-10.9-37 0L165.1 689c-12.2 14.2-1.2 35 18.5 35h656.8c19.7 0 30.7-20.8 18.5-35z" />
+            </svg>
+          </span>
+          <span className={isFilterIncrease === false ? 'active' : 'disabled'}>
+            <svg viewBox="0 0 1024 1024" focusable="false" width="1em" height="1em">
+              <path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z" />
+            </svg>
+          </span>
+        </span>
+      </div>
+    )}
+    </FilterContext.Consumer>
+  );
+};
+
+export default HeaderCell;
---
+++ src/components/TableHeader/HeaderCell.stories.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import HeaderCell from './HeaderCell';
+import FilterContext from '../filterContext';
+
+import './TableHeader.scss';
+
+export default {
+  title: 'Header Cell',
+  component: HeaderCell,
+};
+
+export const Base = () => {
+  const [activeFilter, setActiveFilter] = React.useState(
+    {
+      activeClassCell: '',
+      increase: true,
+    },
+  );
+  return (
+    <FilterContext.Provider
+      value={{ activeFilter, toggleActiveFilter: setActiveFilter }}
+    >
+      <HeaderCell cellClass='cell_id' label='ID' />
+    </FilterContext.Provider>
+  );
+};
+
+export const TwoCellTogether = () => {
+  const [activeFilter, setActiveFilter] = React.useState(
+    {
+      activeClassCell: '',
+      increase: true,
+    },
+  );
+  return (
+    <FilterContext.Provider
+      value={{ activeFilter, toggleActiveFilter: setActiveFilter }}
+    >
+      <HeaderCell cellClass='cell_id' label='ID' />
+      <HeaderCell cellClass='cell_first_name' label='First Name' />
+    </FilterContext.Provider>
+  );
+};
